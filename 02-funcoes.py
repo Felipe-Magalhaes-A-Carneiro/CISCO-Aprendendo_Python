df1bcde6bd9308b711d6b34dc9cfcd740dd9b23d
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2025-09-09 16:14:00 -0300

    aula 1 e 2

---
+++ 02-funcoes.py
@@ -0,0 +1,14 @@
+print() # É uma função que imprime algo na tela.
+
+print("Hello, Word!") # Imprime a string "Hello, Word!" na tela.
+
+# Função: É um bloco de código reutilizável que aceita entradas, processa essas entradas e retorna uma saída. Algumas funções podem não retornar nada (void), como o caso do print().
+
+# Dentro dos parênteses, podemos passar 'argumentos' (valores) para a função processar.
+
+print("A pequenina aranha escalou a tromba d'água.")
+print("Caiu a chuva e lavou a aranha.")
+
+# Exemplo de função com múltiplos argumentos
+# Em Python, duas funções não podem ter duas funções ocupando a mesma posição.
+
